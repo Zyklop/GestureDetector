6111c42020ed5923d4a654eaa35d93fb07587d3f
Renato Bosshart <rbosshar@hsr.ch>
2012-10-26 09:09:07 +0200

    Static + Dynamic Conditions

---
+++ Projekte/Gesture Recognition Renato/Gesture Detector/DynamicCondition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataSources;
+using GestureEvents;
+
+namespace Conditions
+{
+    class DynamicCondition:ICondition
+    {
+        protected Person person;
+
+        public DynamicCondition(Person p) 
+        { 
+            person = p;
+            person.NewSkeleton += check;
+        }
+
+        public event EventHandler Succeded;
+
+        //protected virtual void fireSucceded(object src, EventArgs e)
+        //{
+        //    Succeded(src, e);
+        //}
+
+        public event EventHandler Failed;
+
+        //protected virtual void fireFailed(object src, EventArgs e)
+        //{
+        //    Failed(src, e);
+        //}
+
+        virtual void check(object src, NewSkeletonEventArg e)
+        {
+            // check
+        }
+    }
+}
