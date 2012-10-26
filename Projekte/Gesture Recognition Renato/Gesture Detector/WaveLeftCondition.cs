adb3548f3c8bb9312c8276c633b475db4f085f09
schmijos <j1schmid@hsr.ch>
2012-10-26 12:13:44 +0200

    WaveLeft und WaveRight Conditions erstellt in neuem Namespace

---
+++ Projekte/Gesture Recognition Renato/Gesture Detector/WaveLeftCondition.cs
@@ -0,0 +1,41 @@
+﻿using DataSources;
+using GestureEvents;
+using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Conditions.Login
+{
+    class WaveLeftCondition: DynamicCondition
+    {
+        private Checker checker;
+
+        public WaveLeftCondition(Person p): base(p)
+        {
+            checker = new Checker(p);
+        }
+
+        private void check(object src, NewSkeletonEventArg e)
+        {
+            List<Direction> rightHandDirections = checker.GetAbsoluteMovement(JointType.HandRight);
+            List<Direction> handToHeadDirections = checker.GetRelativePosition(JointType.Head, JointType.HandRight);
+
+            rightHandDirections.ForEach(delegate(Direction x) { Debug.Write(x.ToString()); });
+
+            // Prüfe ob Handbewegung nach links abläuft und ob sich die Hand über dem Kopf befindet
+            if (rightHandDirections.Contains(Direction.left) && handToHeadDirections.Contains(Direction.upward))
+            {
+                Debug.WriteLine("WaveLeft recognized.");
+                Succeded(this);
+            }
+            else
+            {
+                Failed(this);
+            }
+        }
+    }
+}
