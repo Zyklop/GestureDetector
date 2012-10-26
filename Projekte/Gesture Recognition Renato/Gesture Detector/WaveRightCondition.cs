bf6711020e0eb8ad5b58b7de4c46aed278564e55
schmijos <j1schmid@hsr.ch>
2012-10-26 15:13:43 +0200

    Elementare Implementierung von Wave Parts

--- Projekte/Gesture Recognition Renato/Gesture Detector/WaveRightCondition.cs
+++ Projekte/Gesture Recognition Renato/Gesture Detector/WaveRightCondition.cs
@@ -12,26 +12,36 @@ namespace Conditions.Login
 {
     class WaveRightCondition: DynamicCondition
     {
+        private const int LOWER_BOUND_FOR_SUCCESS = 5;
+        private int index = 0;
         private Checker checker;
+        List<Direction> rightHandDirections, handToHeadDirections;
 
         public WaveRightCondition(Person p): base(p)
         {
             checker = new Checker(p);
         }
 
-        protected override void check(object src, NewSkeletonEventArg e)
+        protected override void check(object sender, NewSkeletonEventArg e)
         {
-            List<Direction> rightHandDirections = checker.GetAbsoluteMovement(JointType.HandRight);
-            List<Direction> handToHeadDirections = checker.GetRelativePosition(JointType.Head, JointType.HandRight);
+            rightHandDirections = checker.GetAbsoluteMovement(JointType.HandRight);
+            handToHeadDirections = checker.GetRelativePosition(JointType.Head, JointType.HandRight);
 
+            // TODO Debugoutput entfernen
             rightHandDirections.ForEach(delegate(Direction x) { Debug.Write(x.ToString()); });
 
             // Prüfe ob Handbewegung nach links abläuft und ob sich die Hand über dem Kopf befindet
             if (rightHandDirections.Contains(Direction.right) && handToHeadDirections.Contains(Direction.upward))
             {
                 fireTriggered(this, null);
-                Debug.WriteLine("WaveLeft recognized.");
-                //fireRecognized(this, null);
+                Debug.Write("T");
+
+                index++;
+                if (index >= LOWER_BOUND_FOR_SUCCESS)
+                {
+                    fireSucceeded(this, null);
+                    Debug.Write("S");
+                }
             }
         }
     }
