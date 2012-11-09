7120b1dd02cfa386e4ffddf2c4aff2d6e8534a64
schmijos <j1schmid@hsr.ch>
2012-11-09 11:42:27 +0100

    fehlende Files commited

---
+++ Projekte/Gesture Recognition Renato/Gesture Detector/GestureEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MF.Engineering.MF8910.GestureDetector.Events
+{
+    abstract class GestureEventArgs: EventArgs
+    {
+    }
+}
---
+++ Projekte/Gesture Recognition Renato/Gesture Detector/ZoomGestureEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using MF.Engineering.MF8910.GestureDetector.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MF.Engineering.MF8910.GestureDetector.Gestures.Zoom
+{
+    class ZoomGestureEventArgs: GestureEventArgs
+    {
+        public double Gauge { get; set; }
+    }
+}
