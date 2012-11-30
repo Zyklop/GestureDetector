a4ff6a2e5e1f82f944491a18efb39737ac22f4be
schmijos <j1schmid@hsr.ch>
2012-11-30 14:24:54 +0100

    CurrentMillis hinzugefügt

---
+++ Projekte/Gesture Recognition Renato/Gesture Detector/CurrentMillis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MF.Engineering.MF8910.GestureDetector.DataSources
+{
+    static class CurrentMillis
+    {
+        private static DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long Millis { get { return (long)((DateTime.UtcNow - Jan1st1970).TotalMilliseconds); } }
+    }
+}
