803ee02c244ceccc1d7789de15634ff8007192ba
schmijos <j1schmid@hsr.ch>
2012-11-30 16:23:28 +0100

    Kommentiert

--- Projekte/Gesture Recognition Renato/Gesture Detector/Condition.cs
+++ Projekte/Gesture Recognition Renato/Gesture Detector/Condition.cs
@@ -8,31 +8,47 @@ using MF.Engineering.MF8910.GestureDetector.Events;
 
 namespace MF.Engineering.MF8910.GestureDetector.Gestures
 {
+    /**
+     * Base class for static gesture parts. They just succeed or fail.
+     */
     public abstract class Condition
     {
         protected Person person;
 
+        /**
+         * Create a gesture part, whose fullfillment is checked on Person p.
+         */
         public Condition(Person p) 
         { 
             person = p;
         }
 
+        /**
+         * Begin checking new skeletons.
+         * Save performance and enable only gestures you really need to check.
+         */
         public void enable()
         {
             person.NewSkeleton += check;
         }
 
+        /**
+         * Dont react on new skeletons (anymore).
+         * Use this to save performance after a gesture isn't used anymore.
+         */
         public void disable()
         {
             person.NewSkeleton -= check;
         }
 
+        /**
+         * Implement this to check for a gesture part.
+         */
         protected abstract void check(object src, NewSkeletonEventArgs e);
 
         #region Events
 
         public event EventHandler<GestureEventArgs> Succeeded;
-
         public event EventHandler<FailedGestureEventArgs> Failed;
 
         protected void fireSucceeded(object sender, GestureEventArgs e)
