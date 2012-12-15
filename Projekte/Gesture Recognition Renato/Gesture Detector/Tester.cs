3b0eeed50e6f34b661056f4ab1663172081b1835
Renato Bosshart <rbosshar@hsr.ch>
2012-12-15 13:11:36 +0100

    Beispielcode

--- Projekte/Gesture Recognition Renato/Gesture Detector/Tester.cs
+++ Projekte/Gesture Recognition Renato/Gesture Detector/Tester.cs
@@ -1,24 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
 using MF.Engineering.MF8910.GestureDetector.DataSources;
 using MF.Engineering.MF8910.GestureDetector.Events;
+using MF.Engineering.MF8910.GestureDetector.Gestures;
+using MF.Engineering.MF8910.GestureDetector.Gestures.Swipe;
+using MF.Engineering.MF8910.GestureDetector.Tools;
+using Microsoft.Kinect;
+using System.Linq;
 
 namespace MF.Engineering.MF8910.GestureDetector
 {
     class Tester
     {
+        private static JumpGestureChecker jgc;
+
         static void Main(string[] args)
         {
-            Device d = new Device();
-            d.NewPerson += NewPerson;
-            d.Start();
+            Device d = new Device(); // Erstellen des des Devices
+            d.NewPerson += NewPerson; // Registrieren auf neue Personen
+            d.Start(); // Starten der Kinect
 
             Console.ReadLine();
         }
 
-        static void NewPerson(object src, NewPersonEventArgs e)
+        static void NewPerson(object src, NewPersonEventArgs newPersonEventArgs )
         {
-            e.Person.OnWave += delegate { Console.Write("!"); };
-            Console.WriteLine(e.Person.Id);
+            jgc = new JumpGestureChecker(newPersonEventArgs.Person);
+            jgc.Successful += delegate { Console.WriteLine("Jump"); };
+
         }
     }
+
+    class JumpGestureChecker : GestureChecker
+    {
+        public JumpGestureChecker(Person p) : base(new List<Condition>() { new JumpCondition(p)}, 1000)
+        {
+        }
+    }
+
+    class JumpCondition : Condition
+    {
+        private Checker c;
+
+        public JumpCondition(Person p) : base(p)
+        {
+            c = new Checker(p);
+        }
+
+        protected override void Check(object src, NewSkeletonEventArgs e)
+        {
+            if (c.GetAbsoluteMovement(JointType.HipCenter).Contains(Direction.Upward))
+            {
+                FireSucceeded(this, new JumpGestureEventArgs());
+            }
+            else
+            {
+                FireFailed(this, new FailedGestureEventArgs{Condition = this});
+            }
+        }
+    }
+
+    class JumpGestureEventArgs : GestureEventArgs{}
 }
